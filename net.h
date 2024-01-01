197d6cc3d537fbeca3c0c746b7b94f0df6a1f610
hiroaqii <hiroaqii@gmail.com>
2024-01-01 15:48:57 +0900

    Add .vscode/settings.json and update code formatting

--- net.h
+++ net.h
@@ -8,15 +8,15 @@
 #define IFNAMSIZ 16
 #endif
 
-#define NET_DEVICE_TYPE_DUMMY     0x0000
-#define NET_DEVICE_TYPE_LOOPBACK  0x0001
-#define NET_DEVICE_TYPE_ETHERNET  0x0002
+#define NET_DEVICE_TYPE_DUMMY 0x0000
+#define NET_DEVICE_TYPE_LOOPBACK 0x0001
+#define NET_DEVICE_TYPE_ETHERNET 0x0002
 
-#define NET_DEVICE_FLAG_UP        0x0001
-#define NET_DEVICE_FLAG_LOOPBACK  0x0010
+#define NET_DEVICE_FLAG_UP 0x0001
+#define NET_DEVICE_FLAG_LOOPBACK 0x0010
 #define NET_DEVICE_FLAG_BROADCAST 0x0020
-#define NET_DEVICE_FLAG_P2P       0x0040
-#define NET_DEVICE_FLAG_NEED_ARP  0x0100
+#define NET_DEVICE_FLAG_P2P 0x0040
+#define NET_DEVICE_FLAG_NEED_ARP 0x0100
 
 #define NET_DEVICE_ADDR_LEN 16
 
@@ -44,24 +44,20 @@ struct net_device {
 struct net_device_ops {
     int (*open)(struct net_device *dev);
     int (*close)(struct net_device *dev);
-    int (*transmit)(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst);
+    int (*transmit)(struct net_device *dev, uint16_t type, const uint8_t *data,
+                    size_t len, const void *dst);
 };
 
-extern struct net_device *
-net_device_alloc(void);
-extern int
-net_device_register(struct net_device *dev);
-extern int
-net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst);
+extern struct net_device *net_device_alloc(void);
+extern int net_device_register(struct net_device *dev);
+extern int net_device_output(struct net_device *dev, uint16_t type,
+                             const uint8_t *data, size_t len, const void *dst);
 
-extern int
-net_input_handler(uint16_t type, const uint8_t *data, size_t len, struct net_device *dev);
+extern int net_input_handler(uint16_t type, const uint8_t *data, size_t len,
+                             struct net_device *dev);
 
-extern int
-net_run(void);
-extern void
-net_shutdown(void);
-extern int
-net_init(void);
+extern int net_run(void);
+extern void net_shutdown(void);
+extern int net_init(void);
 
 #endif
