89f100426589d7d793e5a5160a0425a8496cee98
hiroaqii <hiroaqii@gmail.com>
2024-01-02 16:09:01 +0900

    Complete step4

---
+++ ip.c
@@ -0,0 +1,21 @@
+#include "ip.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "net.h"
+#include "util.h"
+
+static void ip_input(const uint8_t *data, size_t len, struct net_device *dev) {
+    debugf("dev=%s, len=%zu", dev->name, len);
+    debugdump(data, len);
+}
+
+// プロトコルスタックにIPの入力関数を登録
+int ip_init(void) {
+    if (net_protocol_register(NET_PROTOCOL_TYPE_IP, ip_input) == -1) {
+        errorf("net_protocol_register() failure");
+        return -1;
+    }
+    return 0;
+}
