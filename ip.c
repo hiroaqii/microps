a20d04e294d4c9ed844bc8fff51e7d50e23cc4cd
hiroaqii <hiroaqii@gmail.com>
2024-01-09 22:27:25 +0900

    Complete step8

--- ip.c
+++ ip.c
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
 
 #include "net.h"
 #include "platform.h"
@@ -227,6 +229,114 @@ static void ip_input(const uint8_t *data, size_t len, struct net_device *dev) {
            ip_addr_ntop(iface->unicast, addr, sizeof(addr)), hdr->protocol,
            total);
     ip_dump(data, total);
+}
+
+static int ip_output_device(struct ip_iface *iface, const uint8_t *data,
+                            size_t len, ip_addr_t dst) {
+    uint8_t hwaddr[NET_DEVICE_ADDR_LEN] = {};
+
+    // ARP によるアドレス解決が必要なデバイスのための処理
+    if (NET_IFACE(iface)->dev->flags & NET_DEVICE_FLAG_NEED_ARP) {
+        // 宛先がブロードキャストIPアドレスの場合には
+        // ARPによるアドレス解決は行わずにそのデバイスのブロードキャストHWアドレスを使う
+        if (dst == iface->broadcast || dst == IP_ADDR_BROADCAST) {
+            memcpy(hwaddr, NET_IFACE(iface)->dev->broadcast,
+                   NET_IFACE(iface)->dev->alen);
+        } else {
+            // まだ ARP を実装していないのでエラーにしておく
+            errorf("arp does not implement");
+            return -1;
+        }
+    }
+    return net_device_output(NET_IFACE(iface)->dev, NET_PROTOCOL_TYPE_IP, data,
+                             len, hwaddr);
+}
+
+static ssize_t ip_output_core(struct ip_iface *iface, uint8_t protocol,
+                              const uint8_t *data, size_t len, ip_addr_t src,
+                              ip_addr_t dst, uint16_t id, uint16_t offset) {
+    uint8_t buf[IP_TOTAL_SIZE_MAX];
+    struct ip_hdr *hdr;
+    uint16_t hlen, total;
+    char addr[IP_ADDR_STR_LEN];
+
+    hdr = (struct ip_hdr *)buf;
+    // IPヘッダの各フィールドに値を設定
+    hlen = IP_HDR_SIZE_MIN;
+    hdr->vhl = (IP_VERSION_IPV4 << 4) | (hlen >> 2);
+    hdr->tos = 0;
+    total = hlen + len;
+    hdr->total = hton16(total);
+    hdr->id = hton16(id);
+    hdr->offset = hton16(offset);
+    hdr->ttl = 0xff;
+    hdr->protocol = protocol;
+    hdr->sum = 0;
+    hdr->src = src;
+    hdr->dst = dst;
+    hdr->sum = cksum16((uint16_t *)hdr, hlen, 0);
+    memcpy(hdr + 1, data, len);
+    debugf("dev=%s, dst=%s, protocol=%u, len=%u", NET_IFACE(iface)->dev->name,
+           ip_addr_ntop(dst, addr, sizeof(addr)), protocol, total);
+    ip_dump(buf, total);
+    // 生成したIPデータグラムを実際にデバイスから送信するための関数に渡す
+    return ip_output_device(iface, buf, total, dst);
+}
+
+static uint16_t ip_generate_id(void) {
+    static mutex_t mutex = MUTEX_INITIALIZER;
+    static uint16_t id = 128;
+    uint16_t ret;
+
+    mutex_lock(&mutex);
+    ret = id++;
+    mutex_unlock(&mutex);
+    return ret;
+}
+
+ssize_t ip_output(uint8_t protocol, const uint8_t *data, size_t len,
+                  ip_addr_t src, ip_addr_t dst) {
+    struct ip_iface *iface;
+    char addr[IP_ADDR_STR_LEN];
+    uint16_t id;
+
+    if (src == IP_ADDR_ANY) {
+        // まだIPルーティングを実装していないので送信元IPアドレスが指定されない場合はエラーを返す
+        errorf("ip routing does not implement");
+        return -1;
+    } else {
+        // 送信元IPアドレス（src）に対応するIPインタフェースを検索
+        iface = ip_iface_select(src);
+        if (!iface) {
+            errorf("iface not found, src=%s",
+                   ip_addr_ntop(src, addr, sizeof(addr)));
+            return -1;
+        }
+
+        // 宛先へ到達可能か確認
+        if ((dst & iface->netmask) != (iface->unicast & iface->netmask) &&
+            dst != IP_ADDR_BROADCAST) {
+            errorf("address not reached, dst=%s",
+                   ip_addr_ntop(src, addr, sizeof(addr)));
+            return -1;
+        }
+    }
+
+    // フラグメンテーションをサポートしないのでMTUを超える場合はエラーを返す
+    if (NET_IFACE(iface)->dev->mtu < IP_HDR_SIZE_MIN + len) {
+        errorf("too long, dev=%s, mtu=%u < %zu", NET_IFACE(iface)->dev->name,
+               NET_IFACE(iface)->dev->mtu, IP_HDR_SIZE_MIN + len);
+        return -1;
+    }
+    // IPデータグラムのIDを採番
+    id = ip_generate_id();
+    // IPデータグラムを生成して出力するための関数を呼び出す
+    if (ip_output_core(iface, protocol, data, len, iface->unicast, dst, id,
+                       0) == -1) {
+        errorf("ip_output_core() failure");
+        return -1;
+    }
+    return len;
 }
 
 // プロトコルスタックにIPの入力関数を登録
