78d7394a4eef8d6563ec0590d6fa2902c836ccff
hiroaqii <hiroaqii@gmail.com>
2024-01-14 07:23:31 +0900

    Complete step11

--- icmp.c
+++ icmp.c
@@ -2,9 +2,12 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "ip.h"
 #include "util.h"
+
+#define ICMP_BUFSIZ IP_PAYLOAD_SIZE_MAX
 
 struct icmp_hdr {
     uint8_t type;
@@ -98,6 +101,42 @@ void icmp_input(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst,
     debugf("%s => %s, len=%zu", ip_addr_ntop(src, addr1, sizeof(addr1)),
            ip_addr_ntop(dst, addr2, sizeof(addr2)), len);
     icmp_dump(data, len);
+    switch (hdr->type) {
+        case ICMP_TYPE_ECHO:
+            // 送信元はEchoメッセージを受信したインタフェース（iface）のユニキャストアドレス
+            // あて先は Echoメッセージの送信元（src）
+            icmp_output(ICMP_TYPE_ECHOREPLY, hdr->code, hdr->values,
+                        (uint8_t *)(hdr + 1), len - sizeof(*hdr),
+                        iface->unicast, src);
+            break;
+        default:
+            break;
+    }
+}
+
+int icmp_output(uint8_t type, uint8_t code, uint32_t values,
+                const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst) {
+    uint8_t buf[ICMP_BUFSIZ];
+    struct icmp_hdr *hdr;
+    size_t msg_len;  // ICMPメッセージの長さ（ヘッダ＋データ）
+    char addr1[IP_ADDR_STR_LEN];
+    char addr2[IP_ADDR_STR_LEN];
+
+    hdr = (struct icmp_hdr *)buf;
+    hdr->type = type;
+    hdr->code = code;
+    hdr->sum = 0;
+    hdr->values = values;
+    memcpy(hdr + 1, data, len);
+    msg_len = sizeof(*hdr) + len;
+    hdr->sum = cksum16((uint16_t *)hdr, msg_len, 0);
+
+    debugf("%s => %s, len=%zu", ip_addr_ntop(src, addr1, sizeof(addr1)),
+           ip_addr_ntop(dst, addr2, sizeof(addr2)), msg_len);
+    icmp_dump((uint8_t *)hdr, msg_len);
+
+    // IPの出力関数を呼び出してメッセージを送信
+    return ip_output(IP_PROTOCOL_ICMP, buf, msg_len, src, dst);
 }
 
 int icmp_init(void) {
